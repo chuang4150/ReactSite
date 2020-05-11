978ebc4dc00878ee03a9f86f5587aefc8005ce21
Chris Huang <chuang4150@gmail.com>
2020-05-10 23:13:08 -0500

    Updates

---
+++ precache-manifest.2fe8a49a023c5285cbfb23632835c035.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdb817e166ce18e20d7864ab0fc314bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a9e8710787068495110",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "6fb73c8be292b7589542",
+    "url": "/static/css/main.5203011d.chunk.css"
+  },
+  {
+    "revision": "1a9e8710787068495110",
+    "url": "/static/js/2.8eabbe43.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.8eabbe43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fb73c8be292b7589542",
+    "url": "/static/js/main.c687bac3.chunk.js"
+  },
+  {
+    "revision": "93329eb38b5855ec27de",
+    "url": "/static/js/runtime-main.52bf020e.js"
+  },
+  {
+    "revision": "9d1fd10f390614b0f9bb797d0d361b7a",
+    "url": "/static/media/image1.9d1fd10f.jpeg"
+  },
+  {
+    "revision": "74adb9fde8bea0e4cd7ad98efd32d018",
+    "url": "/static/media/pic1.74adb9fd.jpg"
+  },
+  {
+    "revision": "50ad35972f5d2564eda401cb7dea2938",
+    "url": "/static/media/propic.50ad3597.jpg"
+  }
+]);
